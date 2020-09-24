11134fff1603c19bf5edbc144c3eca3b4ef5bf0e
umid <undigitals@gmail.com>
2020-09-24 11:29:42 +0900

    fragments

--- src/FeedContainer.tsx
+++ src/FeedContainer.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { useUniverNofrQuery } from './apollo/types'
+import { useUniverQuery, UniversityFragment } from './apollo/types'
 import Feed from './Feed'
 
 export default function FeedContainer() {
-    const {loading, data,error} = useUniverNofrQuery()
+    const {loading, data,error} = useUniverQuery()
     console.log('data', data)
     console.log('error', error)
     console.log('loading', loading)
     
     if (!loading){
         return (
-            <Feed nameEng={data?.universities}/>
+            <Feed nameEng={data?.universities as UniversityFragment[] || []}/>
         )
     }
-    
+    return null
 }
